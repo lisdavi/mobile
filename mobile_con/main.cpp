cb6b8de4ce11db39796ff5e89da75716159d54f1
lisdavi <153983285+lisdavi@users.noreply.github.com>
2023-12-25 15:20:09 +0300

    Update main.cpp

--- mobile_con/main.cpp
+++ mobile_con/main.cpp
@@ -2,6 +2,9 @@
 
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #9. Telephone conversations\n";
+    cout << "Author: Nabieva Anastasia\n";
+   
     return 0;
 }
